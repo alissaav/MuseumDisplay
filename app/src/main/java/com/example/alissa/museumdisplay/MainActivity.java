79296be5e852e047b24fe26655c23cac7631de10
alissaav <alissa.avt@gmail.com>
2018-12-07 19:44:53 +0100

    PENIS

--- app/src/main/java/com/example/alissa/museumdisplay/MainActivity.java
+++ app/src/main/java/com/example/alissa/museumdisplay/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button1 = (Button)findViewById(R.id.button1);
+        Button button1 = findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
